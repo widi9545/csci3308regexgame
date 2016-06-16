e40aaddf6416fadbab1f1e2e2fffa28c1a5f71d9
widi9545 <widi9545@colorado.edu>
2016-06-15 22:23:23 -0600

    A basic DB implementation

---
+++ testDbImplen.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+import MySQLdb
+from playerClass import player
+import random
+
+
+
+db = MySQLdb.connect("127.0.0.1", "root", "root", "questions")
+cur = db.cursor()
+questions =[]
+
+
+
+
+def dbRead():
+	for dbread in cur.fetchone():
+		questions.append(dbread)
+		
+
+
+
+def gameDeclaration():
+	testPlayer = player()
+	#Questions go in as ('a',) what can we do about this?
+	control = "Yes"
+	listRead = 0
+	
+	
+	while(control == "Yes"):
+		checkInput = input("Enter what the first expression is")
+		cur.execute("SELECT Question FROM easy")
+		dbRead()
+		
+		
+		if(checkInput == questions[listRead]): #check first expression 
+			print "You have entered the correct expression!"
+			testPlayer.addScore()
+			testPlayer.printScore()
+			contPlay = input("Would you like to play again? Type 'Yes' to continue, type 'No' to quit\n")
+			if(contPlay == "No"):
+				return 1
+			if(contPlay == "Yes"):
+				control = "Yes"
+				listRead = listRead + 1
+				dbRead()
+		else:
+			print "False! Try again please"
+gameDeclaration()
